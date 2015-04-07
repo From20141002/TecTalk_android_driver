c752325585f0efa3d60be0990d776569bfb60b5e
mysinta <asinta@naver.com>
2015-04-07 22:02:19 +0900

    테스트 Log 입력

--- android_driver/src/com/tectalk/tectalk_driver/MainActivity.java
+++ android_driver/src/com/tectalk/tectalk_driver/MainActivity.java
@@ -2,6 +2,7 @@ package com.tectalk.tectalk_driver;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,6 +18,7 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		Log.d("aaa","test");
 		return true;
 	}
 
